8540b783e07bcaae890188299ca1e69e5d7b735b
SantiagoG117 <garc0254@algonquinlive.com>
2025-04-02 09:39:47 -0400

    Built responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorPalette="blue">Button</Button>;
+  // hook that returns a boolean value based on the current screen size
+  const isAboveLg = useBreakpointValue({ base: false, lg: true }); //Values for a screen bigger than 1024px
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={isAboveLg}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
