f42b599d52cda92722cb8a303b9104249ed0cc05
SantiagoG117 <garc0254@algonquinlive.com>
2025-04-02 09:58:21 -0400

    Built navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   // hook that returns a boolean value based on the current screen size
@@ -12,8 +13,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show when={isAboveLg}>
         <GridItem area="aside" bg="gold">
