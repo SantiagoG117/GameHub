5fc881b1428adaab1a996b21e0293698590cfe63
SantiagoG117 <garc0254@algonquinlive.com>
2025-08-07 13:41:53 -0400

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,38 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+  text: string;
+}
+
+function ExpandableText({ text }: Props) {
+  const [isExpandable, setIsExpandable] = useState(true);
+
+  if (!text) return null;
+
+  //Make sure the text is long enought
+  const isLong = text.length >= 400;
+
+  //Dynamically set the text to be displayed
+  const displayText =
+    isExpandable && isLong ? text.slice(0, 400) + "..." : text;
+
+  return (
+    <Text>
+      {displayText}{" "}
+      <Button
+        size="xs"
+        fontWeight="bold"
+        colorScheme="yellow"
+        bg="yellow.400"
+        color="black"
+        _hover={{ bg: "yellow.500" }}
+        onClick={() => setIsExpandable(!isExpandable)}
+      >
+        {isExpandable ? "Show more" : "Show less"}
+      </Button>
+    </Text>
+  );
+}
+
+export default ExpandableText;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,18 +1,30 @@
+import ExpandableText from "@/components/ExpandableText";
 import useGame from "@/hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
-import React from "react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function GameDetailPage() {
   //Extract params values from the URL
   const params = useParams();
   const { data, isLoading, error } = useGame(params.gameName);
+  const [isCut, setIsCut] = useState(true);
+
+  //Make sure the description is long enough
+  const isLongDescription =
+    data?.description_raw && data.description_raw.length >= 400;
+
+  //Set the dynamic text for the description
+  const description =
+    isCut && isLongDescription
+      ? data.description_raw.slice(0, 400) + "... "
+      : data?.description_raw;
 
   if (isLoading) return <Spinner />;
   return (
     <>
       <Heading>{data?.name}</Heading>
-      <Text>{data?.description_raw}</Text>
+      <ExpandableText text={data?.description_raw || ""} />
     </>
   );
 }
