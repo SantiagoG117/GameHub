820c71311d8c799d1995bfebd6a4e374e8ff5375
SantiagoG117 <garc0254@algonquinlive.com>
2025-08-20 15:34:45 -0400

    Set up environment variables for accessing RAWG API

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -6,7 +6,7 @@ import { FetchedData } from "../entities/FetchedData";
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "a3d10e5f73d84fd9a53b2cd681320958", //The key will be included in the query string of every HTTP request
+    key: import.meta.env.VITE_RAWG_API_KEY, //The key will be included in the query string of every HTTP request
   },
 });
 
